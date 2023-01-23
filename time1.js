11ade797f9c3826ccb5132dd94e3b4a0af642dfe
seemabibi <seema.bibi@hotmail.co.uk>
2023-01-23 21:04:20 +0000

    fixed bug

--- time1.js
+++ time1.js
@@ -1,7 +1,9 @@
 var options = {
   starHour: 9,
-  endHour: 15,
+  endHour: 17,
 }
+
+// funtion to save the informaton entered to local storage 
 function newTimeslots() {
 
     var currentHour = moment().hour();
@@ -24,6 +26,26 @@ function newTimeslots() {
 
     });
 }
+
+//create for loop to go through and creates all elements for the UI
+
+for (var hour = times.startHour; hour <= times.endHour; hour++) {
+    var storedItem = localStorage.getItem(hour);
+    var timeSlot = $("<div>").addClass("row time-block"); 
+    timeSlot.attr("data-hour", hour);
+    var timeHour = $("<div>")
+      .addClass("col-sm-2 hour")
+      .text(moment(hour, "h").format("h A"));
+    var outerBox = $("<div>").addClass("col-sm-8 row");
+    var enterText = $("<textarea>")
+      .addClass("col-sm-12 description")
+      .val(storedItem);
+    var buttonContainer = $("<div>").addClass(
+      "col-sm-2 btn btn-primary saveBtn d-flex align-items-center justify-content-center"
+    );
+    buttonContainer.on("click", savePlan);
+    var button = $("<button>").addClass("fas fa-save");
+
 newTimeslots();
 
 function onSaveTask (e) {
@@ -38,9 +60,17 @@ function onSaveTask (e) {
 }
 
 // function generateTimeSlots () {
-    // for (var hour = 10; hour <= 15; hour++) }
+    // for (var hour = 10; hour <= 17; hour++) }
 
 // var savedTask = localStorage.getItem(hour);
+function savePlan(e) {
+    var textEntry = $(e.target).parent().find(".description").val(); 
+    var hour = $(e.target).parent().attr("data-hour");
+  
+    //stores text entry to local storage under the key of specific hour 
+    localStorage.setItem(hour, textEntry);
+  
+  
 
 // }
 // Load in the timeslots 
@@ -51,6 +81,8 @@ $('#12 .description').val(localStorage.getItem)("12")
 $('#13 .description').val(localStorage.getItem)("13")
 $('#14 .description').val(localStorage.getItem)("14")
 $('#15 .description').val(localStorage.getItem)("15")
+$('#15 .description').val(localStorage.getItem)("16")
+$('#15 .description').val(localStorage.getItem)("17")
 
 function init () {
     $('.lead').text(today.format("MMM Do, YYYY"));
@@ -62,11 +94,8 @@ function init () {
 
 
  generateTimeslots (){
-    for (var hour=10; hour <= 15; hour++){
+    for (var hour=10; hour <= 17; hour++){
         // load the task from local storage
         var savedTask = localStorage.getItem(hour);
     }
 }
-
-
-
